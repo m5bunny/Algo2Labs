777bdf20539face13d4b15643a2d76bc5fc71805
Yurii Melnychuk <yurii.melnychuk.contact@gmail.com>
2021-12-10 19:57:20 +0100

    Add Heap_sort.h

---
+++ Heap_sort.h
@@ -0,0 +1,14 @@
+#ifndef HEAP_SORT_H_
+#define HEAP_SORT_H_
+#include "Binary_heap_D_A.h"
+
+template<typename T>
+void heap_sort(T * _array, int _n, bool _fix = true, bool(*_comp)(const T &, const T&) = Binary_heap_D_A<T>::def_more_comp)
+{
+    Binary_heap_D_A<int> * bh = new Binary_heap_D_A<int>(_array, _n, _fix, _comp);
+    bh->sort();
+    bh->remove_arr_ptr();
+    delete bh;
+}
+
+#endif
